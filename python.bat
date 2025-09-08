a85689b75e01be3abbb7d94644d05986fe930448
crisfroi <juanfroilanramos@gmail.com>
2025-09-08 04:21:19 +0100

    first commit

---
+++ python.bat
@@ -0,0 +1,11 @@
+@echo off
+pip install Flask
+python -m pip install Flask
+python.exe -m pip install --upgrade pip
+pip install jsons
+python.exe -m pip install flask-sock
+python.exe -m pip install flask-sqlalchemy
+python.exe -m pip install flask-sock flask-sqlalchemy websockets
+python.exe -m pip install pymysql
+echo All packages have been installed successfully.
+pause
